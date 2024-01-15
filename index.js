3bf36b46d8d4a0431a5cbca0c1456b66cbcc2007
emrahkiziltan94 <138603299+emrahkiziltan94@users.noreply.github.com>
2024-01-15 21:35:20 +0300

    commit and push explanation.

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ for(let i = 0;i<sayilar.length;i++){
     tekrarlar[sayilar[i]]++;
   }
 }
-
+console.log("emrah");
 tekraredensayilar = [];
 for(const sayi in tekrarlar){
   if(tekrarlar[sayi]>1){
